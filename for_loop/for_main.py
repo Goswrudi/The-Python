c96b5d2e0606f2f98d23a23ee1feded544e5ce8e
rudransh goswami <rudranshgoswami415@gmail.com>
2026-07-27 17:45:50 +0530

    Created the problem :

--- for_loop/for_main.py
+++ for_loop/for_main.py
@@ -1,3 +1,4 @@
 # Trying to solve some For loops problem 
 
-# Problem 1 : 
+# Problem 1 : Exercise 1: Feed the Zoo Animals (Warm-up)We have a list of hungry animals. Write a loop that loops through the list and prints: "Feeding the Lion", then "Feeding the Elephant", and so on.Your Basket: animals = ["Lion", "Elephant", "Monkey", "Penguin"]Hint: Look back at our very first apple basket example!
+
