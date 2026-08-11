075eaef5026e631d012d0ca39b95b53cfd0427f5
rudransh goswami <rudranshgoswami415@gmail.com>
2026-08-11 23:25:44 +0530

    method 1 (class & object)

---
+++ OOP/oop_basic.py
@@ -0,0 +1,15 @@
+# Class : A class is like a big container in the code which create more resuablity of 
+# Object :  A object is a variable reporudcing factor for the Class 
+# 
+# 
+# Code Example 
+
+class house:
+    house_color = 'pinkish'
+    house_size = 125 , 'M'
+    house_area = 'NCR'
+
+# Using Object to call class M1(stands for Model 1) 
+
+M1 = house()
+print(M1.house_area , M1.house_color , house.house_size)
